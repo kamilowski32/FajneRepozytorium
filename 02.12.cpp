8fb0fec772869165053acb7b4250add0882bf084
kamilowski32 <74541018+kamilowski32@users.noreply.github.com>
2020-12-02 12:20:24 +0100

    Update 02.12.cpp

--- 02.12.cpp
+++ 02.12.cpp
@@ -6,6 +6,8 @@ int main() {
     random_device a;
     mt19937 losowanie(a());
     uniform_int_distribution<> zbior(1,7);
+    for (int i = 0; i < 10; ++i)
+    cout << zior(losowanie)<<endl;
     
     return 0;
 }
