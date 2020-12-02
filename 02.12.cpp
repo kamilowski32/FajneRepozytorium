638f3957c0ec9193c956a18cbc5bf6eef0cb703b
kamilowski32 <74541018+kamilowski32@users.noreply.github.com>
2020-12-02 11:37:37 +0100

    Update 02.12.cpp

--- 02.12.cpp
+++ 02.12.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     random_device a;
     mt19937 losowanie(a());
-    uniform_int_distribution<> zbior(1,6);
+    uniform_int_distribution<> zbior(1,7);
 
     return zbior(losowanie);
 }
